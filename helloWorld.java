3401b321340b61f6162e9e1ba5c39d4755f96736
japhet <japhetseba@gmail.com>
2018-12-17 12:11:42 +0300

    create javadoc for the method

--- helloWorld.java
+++ helloWorld.java
@@ -2,8 +2,12 @@ public class HelloWorld
 {
 	public static void main (String[] args)
 	{
-		System.out.println (makeIntro);
+		System.out.println (makeIntro());
 	}
+	/**
+	* @param introduce a string which describe an individual
+	* @return          description of an individual to the audience
+	*/
 	public static String makeIntro ()
 	{
 		String introduce = "Hello world, I am a programmer!!";
